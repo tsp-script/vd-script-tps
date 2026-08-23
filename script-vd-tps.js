478ae9aeff1d0ec409af4c29b305ae15fa186708
Tps Script <nadute7@gmail.com>
2026-08-23 08:05:59 +0700

    Update script loading URL in script-vd-tps.js

--- script-vd-tps.js
+++ script-vd-tps.js
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/vd-script-8918.js"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/tps-vd.js"))()
