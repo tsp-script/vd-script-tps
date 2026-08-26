29daf0c2f9f635cc9f0e373406229d8492804a13
Tps Script <nadute7@gmail.com>
2026-08-27 04:19:03 +0700

    Update script URL to new version v1.4.1

--- script-vd-tps.js
+++ script-vd-tps.js
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/new-vd-version.js"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/new-version-vd-v1.4.1.js"))()
