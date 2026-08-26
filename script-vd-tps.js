500d2eb8a163bdf42dd7b2ccad3d3ce974366348
Tps Script <nadute7@gmail.com>
2026-08-27 04:05:24 +0700

    Update script to load new version from GitHub

--- script-vd-tps.js
+++ script-vd-tps.js
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/VD-V1.3.0.js"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tsp-script/vd-script-tps/refs/heads/main/new-vd-version.js"))()
